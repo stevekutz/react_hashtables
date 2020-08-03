725b573c788b4ca997f2415ca8665a9bbc64d044
stevekutz <stkutz@gmail.com>
2020-08-02 20:36:10 -0500

    test commit 2

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+inputhandler = () => {
+     
+
+}
+
+
 function App() {
   return (
     <div className="App">
-
+          <input type =  'text' />
     </div>
   );
 }
